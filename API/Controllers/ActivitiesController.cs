e1864e0ea2ad5117999c6d026b883f393808977a
Mohammad Abusalah <55249694+abusamoh25@users.noreply.github.com>
2021-03-18 21:55:43 -0500

    Fixed API not committing

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Domain;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+                return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid Id)
+        {
+            return await _context.Activities.FindAsync(Id);
+        }
+    }
+}
